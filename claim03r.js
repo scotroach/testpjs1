8e1011566b012e53731b42f58fc05b59f4b93920
Scot Roach <scotroach@gmail.com>
2021-07-23 13:04:08 -0500

    Changed RPG program and reconverted another time

--- claim03r.js
+++ claim03r.js
@@ -9,7 +9,7 @@ function claim03r() {
 
   pjs.define("rrn", { type: 'decimal', length: 10, decimals: 0 });
   pjs.define("blahvar", { type: 'char', length: 20 });
-  pjs.define("program", { type: 'char', length: 20 });
+  pjs.define("program", { type: 'char', length: 10 });
   pjs.define("action", { type: 'char', length: 10, varying: true });
   pjs.define("done", { type: 'boolean' });
 
